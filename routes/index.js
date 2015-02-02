1d01d3169c0365b16ff5b0b8654862a7821a08eb
andy.feng <fengyun612@qq.com>
2015-02-02 16:30:21 +0800

    the first commit for init project

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+module.exports = function(app) {
+  app.get('/', function (req, res) {
+    res.render('index', { title: '首页' });
+  });
+  app.get('/products', function (req, res) {
+        res.render('products', { title: '产品中心' });
+  });
+  app.get('/usharp', function (req, res) {
+        res.render('usharp', { title: 'U型节能灯' });
+  });
+  app.get('/reg', function (req, res) {
+    res.render('reg', { title: '注册' });
+  });
+  app.post('/reg', function (req, res) {
+  });
+  app.get('/login', function (req, res) {
+    res.render('login', { title: '登录' });
+  });
+  app.post('/login', function (req, res) {
+  });
+  app.get('/post', function (req, res) {
+    res.render('post', { title: '发表' });
+  });
+  app.post('/post', function (req, res) {
+  });
+  app.get('/logout', function (req, res) {
+  });
+};
